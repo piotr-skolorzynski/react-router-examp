2e4593b7ae762a2dbc8fccc45891ef0239c8dfed
Piotr <p.skolorzynski@gmail.com>
2021-05-18 16:54:28 +0200

    modify Navbar component Import withRouter method to show what Higer Order Component is

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,10 @@
 import React from 'react';
-import { Link, NavLink } from 'react-router-dom';
+import { Link, NavLink, withRouter } from 'react-router-dom';
 
-const Navbar = () => {
+const Navbar = props => {
+        setTimeout(() => {
+        props.history.push('/about')
+    }, 2000)
     return(
         <nav className="nav-wrapper red darken-3">
             <div className="container">
@@ -18,4 +21,4 @@ const Navbar = () => {
 
 // NavLink adds class="active" what gives opportunity to change link's style
 
-export default Navbar;
+export default withRouter(Navbar);
