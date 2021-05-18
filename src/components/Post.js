49e990f9dfde70fe72a472d239637930c78b8789
Piotr <p.skolorzynski@gmail.com>
2021-05-18 23:28:57 +0200

    modify Home and Post components to show individual post after click on post header

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,20 +1,35 @@
+import axios from 'axios';
 import React, { Component } from 'react';
 
 class Post extends Component {
     state = {
-        id: null
+        post: null
     }
     componentDidMount(){
-        console.log(this.props)
+        // console.log(this.props)
         let id = this.props.match.params.post__id; //iside this.props we can find it
-        this.setState({
-            id: id
-        })
+        axios.get('https://jsonplaceholder.typicode.com/posts/' + id)
+            .then(res => {
+                this.setState({
+                    post: res.data
+                })
+            })
+        // console.log(res)
     }
     render() {
+
+        const post = this.state.post ? (
+            <div className="post">
+                <h4 className="center">{this.state.post.title}</h4>
+                <p>{this.state.post.body}</p>
+            </div>
+        ) : (
+            <div className="center">Loading post...</div>
+        )
+
         return (
             <div className="container">
-                <h4>{this.state.id}</h4>
+                { post }
             </div>
         )
     }
