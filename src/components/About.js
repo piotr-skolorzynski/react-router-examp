efee66d683c639e8a694e7c5a2a03d626248819c
Piotr <p.skolorzynski@gmail.com>
2021-05-18 10:38:51 +0200

    add basic structure

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+    return(
+        <div className="container">
+            <h4 className="center">About</h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque nec sapien dolor. Nam ac leo sapien. Curabitur facilisis, ligula ac egestas hendrerit, lorem magna gravida mi, nec efficitur turpis felis bibendum justo. Vestibulum euismod pretium urna et porttitor. Curabitur aliquam dolor luctus lacus auctor tristique. Sed sollicitudin augue risus, ut sollicitudin ex sagittis eu. Nam vel quam posuere sem finibus pellentesque. Maecenas molestie orci vitae metus mattis maximus. Nam a lorem viverra, venenatis arcu eu, auctor purus. Nullam molestie nibh eget augue tristique molestie. Suspendisse vitae augue libero. Donec molestie dolor vel ipsum luctus pretium. Ut nec magna eget dui gravida malesuada. Donec iaculis quam eget augue interdum convallis. Nam ut orci ut enim rutrum condimentum id non diam. Proin venenatis urna semper maximus elementum.</p>
+        </div>
+    )
+}
+
+export default About;
