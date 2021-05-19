133d8d3abcb60aa7d6c953c82f8f6d0830d18e61
Piotr <p.skolorzynski@gmail.com>
2021-05-19 23:40:03 +0200

    add pokeball.png file nest img in Home component in each post modify index.css change style for posts

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
+import Pokeball from '../pokeball.png';
 
 class Home extends Component {
     state = {
@@ -22,12 +23,13 @@ class Home extends Component {
             posts.map(post => {
                return (
                    <div className="post card" key={post.id}>
-                       <div className="card-content">
-                            <Link to={'/' + post.id}>
-                                <span className="card-title">{post.title}</span>
-                            </Link>
-                           <p>{post.body}</p>
-                       </div>
+                    <img src={Pokeball} alt="A pokeball" />
+                    <div className="card-content">
+                        <Link to={'/' + post.id}>
+                            <span className="card-title red-text">{post.title}</span>
+                        </Link>
+                        <p>{post.body}</p>
+                    </div>
                    </div>
                ) 
             })
@@ -36,7 +38,7 @@ class Home extends Component {
         )
         
         return (
-            <div className="container">
+            <div className="home container">
                 <h4 className="center">Home</h4>
                 { postList }
             </div>
